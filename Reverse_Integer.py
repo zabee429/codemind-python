feb634d385dc1b25169f70eff4868662c78524d5
zabee429 <97427630+zabee429@users.noreply.github.com>
2022-05-31 21:16:13 +0530

    n=int(input()) s=0 s1=0 if(n<0):     m=n*(-1)     while(m):        s=s*10+m%10        m=m//10     print("-%d"%s) else:     while(n):         s1=s1*10+n%10         n=n//10     print(s1)

---
+++ Reverse_Integer.py
@@ -0,0 +1,14 @@
+n=int(input())
+s=0
+s1=0
+if(n<0):
+    m=n*(-1)
+    while(m):
+       s=s*10+m%10
+       m=m//10
+    print("-%d"%s)
+else:
+    while(n):
+        s1=s1*10+n%10
+        n=n//10
+    print(s1)
